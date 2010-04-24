22406e35d859ae853aab249f019665d68991fb45
Peter Boothe <peter@Slick.local>
2010-04-24 12:20:25 -0400

    Now calculates fib(4)

--- lisp/fib.lisp
+++ lisp/fib.lisp
@@ -1,1 +1,1 @@
-((fun (f) (f f)) (fun (fib) (fun (n) (if (< n 2) n (+ ((fib fib) (- n 1)) ((fib fib) (- n 2)))))))
+(((fun (f) (f f)) (fun (fib) (fun (n) (if (< n 2) n (+ ((fib fib) (- n 1)) ((fib fib) (- n 2))))))) 4)
